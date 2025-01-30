db521e1f43ae9dd4d0a182fc242b072cb0d7b899
vishalzeni <vishalzenith47@gmail.com>
2025-01-30 18:01:43 +0530

    Update server.jsx cors origin

--- server.jsx
+++ server.jsx
@@ -20,7 +20,7 @@ const port = 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000', // Replace with your frontend URL
+  origin: '*', // Replace with your frontend URL
   methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Content-Type, Authorization',
 }));
